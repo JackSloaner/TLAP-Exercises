9ae6d354cb0e63559a75470438cd35539fc1579f
Jack Sloan <jack12sloan@gmail.com>
2023-05-19 20:21:36 -0400

    Fix 2.6_to_any.cpp to properly answer book question

--- chapters/chap2_puzzles/2.6_to_any/2.6_to_any.cpp
+++ chapters/chap2_puzzles/2.6_to_any/2.6_to_any.cpp
@@ -17,46 +17,61 @@ int findMax(int base){
   return i;
 }
 
-void convert(bool arr[], int max, int b, int num){
-  for (int i = max - 1; i >= 0; i--){
-    int modulo = pow(b, i);
-    int previous = num;
-    num = num % modulo;
-    if (num != previous){
-      arr[(max - 1) - i] = true;
-    } else{
-      arr[(max - 1) - i] = false;
-    }
+int modulo_count(int num, int modulo){
+  int count = 0;
+  while(num >= modulo){
+    num = num - modulo;
+    count++;
   }
+  return count;
+}
 
+void convert(int arr[], int input_num, int in_base, int out_base){
+  int max = findMax(out_base);
+  //Convert input number to base 10 integer
+  int input_converted = 0;
+  for (int i = findMax(in_base) - 1; i >= 0 ; i--){
+    int modulo = pow(10, i);
+    int base_ten_transfer = modulo_count(input_num, modulo);
+    input_num = input_num % modulo;
+    base_ten_transfer = pow(in_base, i) * base_ten_transfer;
+    cout << base_ten_transfer << endl;
+    input_converted += base_ten_transfer;
+  }
+  //Convert base-10 number to output base
+  for (int i = max - 1; i >= 0; i--){
+    int modulo = pow(out_base, i);
+    int count = modulo_count(input_converted, modulo);
+    input_converted = input_converted % modulo;
+    arr[(max - 1) - i] = count;
+  }
   for (int i = 0; i < max; i++){
-    if (arr[i]){
-      cout << 1;
-    } else {
-      cout << 0;
-    }
+    cout << arr[i];
   }
   cout << "\n";
 }
 
-void initializeArray(bool arr[], int max){
+void initializeArray(int arr[], int max){
   for (int i = 0; i < max; i++){
-    arr[i] = false;
+    arr[i] = 0;
   }
 }
 
 int main(){
-  int base;
-  cout << "Enter a base:";
-  cin >> base;
-  int max = findMax(base);
-  int num;
-  cout << "Enter a number to be represented:";
-  cin >> num;
-  bool converted[max];
+  int in_base;
+  cout << "Enter a base for your input number:";
+  cin >> in_base;
+  int input_num;
+  printf("Enter your number in base %i:", in_base);
+  cin >> input_num;
+  int out_base;
+  cout << "Enter a base for your output number:";
+  cin >> out_base;
+  int max = findMax(out_base);
+  int converted[max];
   initializeArray(converted, max);
-  convert(converted, max, base, num);
+  convert(converted, input_num, in_base, out_base);
 }
 
-// Convert any integer number to its closest representation of the number in base b, where the smallest denomination (rightmost unit) is b^0 and the largest is b^(max - 1) (leftmost unit)
+// Convert any integer number to its representation of the number in base b, where the smallest denomination (rightmost unit) is b^0 and the largest is b^(max - 1) (leftmost unit)
 // max is calculated in relation to the base and the maximum integer value.
